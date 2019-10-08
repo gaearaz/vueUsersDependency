cad45dcac3b0307350a62188a862204589c70af1
Juan Diaz <jddiaz82@gmail.com>
2019-10-08 13:19:33 -0500

    add NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,16 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
+    <nav-bar></nav-bar>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import NavBar from "../src/views/NavBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld,
-  },
-  data: () => ({
-    //
-  }),
+    NavBar
+  }
 };
 </script>
---
+++ src/views/NavBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-items>
+      <v-btn flat tag="span" style="cursor: pointer">VUE USERS DEPENDENCY</v-btn>
+    </v-toolbar-items>
+
+    <v-spacer></v-spacer>
+
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn flat v-for="item in items" :key="item.title" :to="item.link">{{item.title}}</v-btn>
+      <v-spacer></v-spacer>
+    </v-toolbar-items>
+
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn flat>Logout</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+export default {
+  computed: {
+    items() {
+      let menuItems = [
+        {
+          title: "Register",
+          icon: "face",
+          link: "/register"
+        },
+        {
+          title: "Login",
+          icon: "no_encryption",
+          link: "/login"
+        }
+      ];
+      return menuItems;
+    }
+  }
+};
+</script>
